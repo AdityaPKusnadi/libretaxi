73ef18c30f1683d8eaaa6f8b3e16e125a629ec60
fahmirizalbudi <fahmirizal.dev@gmail.com>
2026-03-13 14:07:31 +0700

    fix

--- src/response-handlers/cancel-current-order-response-handler.js
+++ src/response-handlers/cancel-current-order-response-handler.js
@@ -19,49 +19,41 @@
 import ResponseHandler from './response-handler';
 import Order from '../order';
 import CaQueue from '../queue/ca-queue';
+import { updateTripStatus } from '../support/oracle-logger';
+import { sendGroupLog, formatDate } from '../support/group-log';
 
-/**
- * Submit order response handler.
- * Creates {@link Order} entity based on {@link SubmitOrderResponse} properties.
- * Sets Order location with geofire package. Geofire updates Order object with
- * `g` and `l` properties. Rest of the properties are copied from {@link SubmitOrderResponse}.
- * `orderKey` is guid.
- *
- * Drivers are subscribed to orders in their area. So when new order within
- * specific area is created, drivers in this area immediately get notified.
- *
- * @author Roman Pushkin (roman.pushkin@gmail.com)
- * @extends {ResponseHandler}
- * @date 2016-08-18
- * @version 1.1
- * @since 0.1.0
- */
 export default class CancelCurrentOrderResponseHandler extends ResponseHandler {
 
-  /**
-   * Constructor.
-   *
-   * @param {Object} response - {@link SubmitOrderResponse} instance.
-   */
   constructor(options) {
     super(Object.assign({ type: 'cancel-current-order-response-handler' }, options));
     this.queue = new CaQueue();
   }
 
-  /**
-   * Handler entry point.
-   * Updates order status to `cancelled` if it's not cancelled. If it's already
-   * cancelled, does nothing. Always calls `onResult`.
-   */
   call(onResult) {
     const orderKey = this.user.state.currentOrderKey;
+    const rideNum = this.user.state.rideNum || null;
+
     new Order({ orderKey }).load().then((order) => {
       if (order.status !== 'cancelled') {
         order.setState({ status: 'cancelled' });
         order.save(() => {
+          if (rideNum) {
+            updateTripStatus(rideNum, { status: 'cancelled' }).catch(() => {});
+
+            const riderName = (this.user.state.identity && this.user.state.identity.username)
+              ? `@${this.user.state.identity.username}`
+              : (this.user.state.identity && this.user.state.identity.first) || 'Rider';
+            const groupLines = [
+              `\u274C Connect \u2014 Ride #${rideNum} Cancelled`,
+              '',
+              `\u{1F464} Rider: ${riderName}`,
+              '',
+              formatDate(),
+            ];
+            sendGroupLog(groupLines.join('\n'));
+          }
+
           this.informPassenger(this.user.userKey);
-          // TODO: inform drivers who were notified already
-          // this.informDrivers(...);
           onResult();
         });
       } else {
@@ -69,14 +61,10 @@ export default class CancelCurrentOrderResponseHandler extends ResponseHandler {
       }
     })
     .catch((err) => {
-      // no idea how to test it, untested code in this block
-      console.log(`Error in CancelCurrentOrderResponseHandler ${err}`); // eslint-disable-line no-console, max-len
+      console.log(`Error in CancelCurrentOrderResponseHandler ${err}`);
     });
   }
 
-  /**
-   * @private
-   */
   informPassenger(userKey) {
     this.queue.redirect({ userKey, route: 'order-cancelled' });
   }
